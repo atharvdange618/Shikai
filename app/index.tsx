47c521b6973fedbf3171d1d6a4bffae9362a1ca1
Atharv Dange <atharvdange618@gmail.com>
2026-04-06 22:38:47 +0530

    feat: initialize app with a basic structure

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#0D1117",
+      }}
+    >
+      <Text style={{ color: "#E6EDF3" }}>Hello World from Shikai! 👋.</Text>
+    </View>
+  );
+}
